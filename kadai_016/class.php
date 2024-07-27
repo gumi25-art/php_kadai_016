f9f4575434311b6799b54e9f81f13f7fe3a862f0
gumi25-art <160398203+gumi25-art@users.noreply.github.com>
2024-07-27 11:53:50 +0800

    Add files via upload

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -2,56 +2,51 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>課題 クラスを2つ作ってそれぞれのインスタンスを出力しよう</title>
+  <title>phpクラス課題</title>
 </head>
 <body>
   <p>
     <?php
-    class Food
-    {
+    class Food {
       private $name;
       private $price;
 
-      public function __construct(string $name, int $price)
-      {
+      public function __construct(string $name, int $price) {
         $this->name = $name;
         $this->price = $price;
       }
 
-      public function getPrice()
-      {
+      public function show_price() {
         return $this->price;
       }
     }
-
-    $food = new Food('potato', 250);
-    echo $food->getPrice();
-    echo "<br>";
-
-
-    class Animal
-    {
+    
+    class Animal {
       private $name;
       private $height;
       private $weight;
 
-      public function __construct(string $name, int $height, int $weight)
-      {
+      public function __construct(string $name, int $height, int $weight) {
         $this->name = $name;
         $this->height = $height;
         $this->weight = $weight;
       }
 
-      public function getHeight()
-      {
+      public function show_height() {
         return $this->height;
       }
     }
-
-    $animal = new Animal('dog', 60, 5000);
-    echo $animal->getHeight();
+    $potato = new Food("potato", 250);
+       $dog = new Animal("dog", 60, 5000);
+       print_r($potato);
+           echo '<br>';
+           print_r($dog);
+           echo '<br>';
+           
+    echo $potato->show_price();
+    echo '<br>';
+    echo $dog->show_height();
     ?>
   </p>
 </body>
 </html>
-
